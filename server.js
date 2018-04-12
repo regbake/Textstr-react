8351902730a06dc7dd47839f910e555d1b42c953
regbake <regbake@gmail.com>
2018-04-11 17:46:26 -0700

    filling out server

--- server.js
+++ server.js
@@ -25,6 +25,51 @@ if (!Object.entries){
   };
 }
 
+app.get("/", function(req, res){
+  res.render("index", {
+    fileArray: [],
+    timeData: [],
+    cleanArray: []
+  });
+});
+
+app.post("/", upload.any("files"), function(req, res){
+  const fileArray = [];
+  const allFiles = req.files; //an array of file Objects if multiple uploaded
+  const files = []; //all the .txtFiles
+  let sortedFrequencyArray, cleanArray, timeData;
+
+  //select only text files
+  allFiles.forEach(function(file){
+    if (file.mimetype === 'text/plain'){
+      files.push(file);
+    }
+  });
+
+  //declare this after cleaning out non-txt files
+  const fileNameArray = TextFunctions.fileNameArray(files);
+
+  if (files.length > 0){
+    sortedFrequencyArray = TextFunctions.fileArrayToFrequencyArray(files); //raw not cleaned
+    cleanArray = TextFunctions.cleanArray(sortedFrequencyArray); //filtered and cleaned, still has \'
+    timeData = TextFunctions.extractTimeData(files.slice(0, (files.length)/2));
+
+    console.log(timeData);
+    // console.log(cleanArray);
+    // console.log(sortedFrequencyArray);
+
+  }
+
+  res.render("index", {
+    fileArray: fileNameArray,
+    //commented out for testing
+    // timeData: [],
+    // cleanArray: []
+    timeData: timeData,
+    cleanArray: cleanArray
+  });
+})
+
 app.get('/api/customers', (req, res) => {
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
