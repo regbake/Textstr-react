5990109e2d028f1da0229f849973de52be542c42
regbake <regbake@gmail.com>
2018-04-11 17:45:07 -0700

    added TextFunction

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ var fs = require("fs");
 var storage = multer.memoryStorage()
 var upload = multer({storage: storage});
 
+//TextFunction module
+const TextFunctions = require('./TextFunctions.js');
+
+//define Object.entries
+if (!Object.entries){
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+    return resArray;
+  };
+}
+
 app.get('/api/customers', (req, res) => {
   const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
